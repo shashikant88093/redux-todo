6cc009519698be42fa04720ff4a90a0735e0b04f
shashikant88093 <shashi94goswami@gmail.com>
2020-06-04 17:22:29 +0530

    Updates

---
+++ precache-manifest.99ef15e008cc6041519147807faedb5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6219c86b25a46666a2df1498ef4aa84c",
+    "url": "/redux-todo/index.html"
+  },
+  {
+    "revision": "e5d55151750f2f8a29d2",
+    "url": "/redux-todo/static/css/main.d5262be1.chunk.css"
+  },
+  {
+    "revision": "544e638bcde95c1c0493",
+    "url": "/redux-todo/static/js/2.0a2ea2dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-todo/static/js/2.0a2ea2dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5d55151750f2f8a29d2",
+    "url": "/redux-todo/static/js/main.e7cfa6a5.chunk.js"
+  },
+  {
+    "revision": "836f13feffbdd1734e8f",
+    "url": "/redux-todo/static/js/runtime-main.b344ca7a.js"
+  }
+]);
